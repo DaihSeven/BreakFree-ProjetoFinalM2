31a9935419c87198346681369dcfa49328d49037
ThalitaSamara <thalitasamara61@gmail.com>
2024-12-11 19:46:16 -0300

    melhorias no desempenho

--- js/team.js
+++ js/team.js
@@ -3,18 +3,34 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.mjs';
 import 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css';
 
- */
+ 
 
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    spaceBetween: 30,
+    slidesPerView: 3,
+    spaceBetween: 25,
     loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor:'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+
+    breakpoints:{
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    }
+  });*/
